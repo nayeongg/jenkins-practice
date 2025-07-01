79edf671680f0e296a7ce3bbaa26da92b85169f0
nayeongg <skdud0422@naver.com>
2025-07-01 13:33:07 +0900

    build.sh 수정

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-echo "✅ 빌드 스크립트 실행됨"
-
+echo "✅ build.sh 실행됨"
 mkdir -p deploy_output
-
-if [ -f index.html ]; then
-    echo "✅ index.html 존재함, 복사 시작"
-    cp index.html deploy_output/
-else
-    echo "❌ index.html 없음"
-    exit 1
-fi
-
-ls -al deploy_output
+cp index.html deploy_output/
